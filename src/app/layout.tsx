883e40fcfca4063abff49fb93ca7f4f01217cd3d
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-15 12:22:09 +0100

    feat: :sparkles: Fixed position for navbar, this makes it so it is always displayed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,15 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <Providers>
-          <NavBar />
-          <main>{children}</main>
+          <div className="min-h-screen flex flex-col">
+            {/* Fixed Navigation */}
+            <div className="fixed top-0 left-0 right-0 z-50">
+              <NavBar />
+            </div>
+
+            {/* Main Content with padding for navbar */}
+            <main className="flex-grow pt-16">{children}</main>
+          </div>
         </Providers>
       </body>
     </html>
