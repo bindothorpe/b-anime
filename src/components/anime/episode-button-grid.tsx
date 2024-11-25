a1c26b37c4e50c7f0ac4a328ce969ff978c34bef
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-25 22:38:06 +0100

    feat: :sparkles: Added max height to episode grid

--- src/components/anime/episode-button-grid.tsx
+++ src/components/anime/episode-button-grid.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import EpisodeButton, { EpisodeButtonProps } from "./episode-button";
 
 export interface EpisodeButtonGridProps {
@@ -8,7 +9,7 @@ export default function EpisodeButtonGrid(props: EpisodeButtonGridProps) {
   const { episodeButtonProps } = props;
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-2">
+    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-2 overflow-y-auto max-h-96">
       {episodeButtonProps.map((ebp) => (
         <EpisodeButton key={ebp.animeId + "-" + ebp.episodeNumber} {...ebp} />
       ))}
