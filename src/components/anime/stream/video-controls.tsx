11611a3c1456f1e6a0531845283ab1f79955e993
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-27 20:51:04 +0100

    refactor: :recycle: Removed unused useState import from video controls component

--- src/components/anime/stream/video-controls.tsx
+++ src/components/anime/stream/video-controls.tsx
@@ -1,5 +1,5 @@
 // VideoControls.tsx
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 
 interface VideoControlsProps {
   videoRef: React.RefObject<HTMLVideoElement>;
